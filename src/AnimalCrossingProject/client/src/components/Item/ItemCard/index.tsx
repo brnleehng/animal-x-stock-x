b57639ee0cbd0a803b73de930751903412dc71c7
quanuw <quanuw@gmail.com>
2020-06-13 19:59:25 -0700

    Add variant color info to item name

--- src/AnimalCrossingProject/client/src/components/Item/ItemCard/index.tsx
+++ src/AnimalCrossingProject/client/src/components/Item/ItemCard/index.tsx
@@ -1,19 +1,16 @@
 import * as React from "react";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
-import Nav from 'react-bootstrap/Nav';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
 import { Item, Variant } from "../../Market";
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 
 
 export class ItemCard extends React.Component<Item & Variant> {
-    constructor(props: Item & Variant) {
-        super(props);
+
+    private getItemVariantInfo(props: Item & Variant): string {
+        if (props.colors && props.colors.length) {
+            return props.colors[0];
+        }
+        return "";
     }
 
     render() {
@@ -22,7 +19,7 @@ export class ItemCard extends React.Component<Item & Variant> {
             <Card className="mx-auto" style={{ width: '10rem' }}>
             <Card.Img variant="top" src={this.props.image} />
             <Card.Body>
-                <Card.Title>{this.props.name}</Card.Title>
+                <Card.Title>{`${this.props.name} (${this.getItemVariantInfo(this.props)})`}</Card.Title>
                 <Card.Text>Lowest Ask</Card.Text>
                 <Card.Text>{this.props.variants[0].sell}</Card.Text>
                 <Card.Link>
@@ -31,7 +28,7 @@ export class ItemCard extends React.Component<Item & Variant> {
                         state: {
                             itemId: `${this.props._id}`,
                             itemUniqueEntryId: `${this.props.uniqueEntryId}`,
-                            itemName: `${this.props.name}`,
+                            itemName: `${this.props.name} (${this.getItemVariantInfo(this.props)})`,
                             itemVariant: `${this.props.variant}`,
                             itemImage: `${this.props.image}`
                         }
